17e8601ddbffd0f92e46cdab131dda6b6c1dd098
Joshua-Edward <109255071+Joshua-Edward@users.noreply.github.com>
2023-03-23 16:09:36 -0700

    Add basic math functions and statement to run operation for given inputs

--- calcJS.js
+++ calcJS.js
@@ -1,9 +1,40 @@
 const add = function(a, b) {
-	let addTotal = a + b;
+	const addTotal = a + b;
   return addTotal;
 };
 
 const subtract = function(a, b) {
-	let subtractTotal = a - b;
+	const subtractTotal = a - b;
   return subtractTotal;
 };
+
+const multiply = function(a, b) {
+  const multiplyTotal = a * b;
+  return multiplyTotal;
+};
+
+const divide = function(a, b) {
+  const divideTotal = a / b;
+  return divideTotal;
+};
+
+let aValue = 0;
+let operatorSelection = 'add';
+let bValue = 0;
+
+const operate = function(aValue, bValue) {
+  const aOperation = aValue;
+  const bOperation = bValue;
+  console.log(typeof(operatorSelection));
+  if (operatorSelection === 'add') {
+    return add(aOperation,bOperation);
+  } else if (operatorSelection === 'subtract') {
+    return subtract(aOperation,bOperation);
+  } else if (operatorSelection === 'multiply') {
+    return multiply(aOperation,bOperation);
+  } else if (operatorSelection === 'divide') {
+    return divide(aOperation,bOperation);
+  };
+};
+
+console.log(operate(12, 3));
